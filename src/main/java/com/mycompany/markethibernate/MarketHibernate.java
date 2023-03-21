305866e85c1f17796d9abdd7beab53a42fb62510
Khacduy <2002nguyenkhacduy@gmail.com>
2023-03-21 20:23:35 +0700

    first Commit

---
+++ src/main/java/com/mycompany/markethibernate/MarketHibernate.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.markethibernate;
+
+/**
+ *
+ * @author duy
+ */
+public class MarketHibernate {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
